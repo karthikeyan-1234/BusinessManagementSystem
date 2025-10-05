91e248f440e65905fb1209532aa24c39b8a568c9
karthikeyan-1234 <mail2karthikkn@gmail.com>
2025-10-05 18:23:02 +0530

    clean up

--- BusinessWeb/src/app/app.component.ts
+++ BusinessWeb/src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component, VERSION } from '@angular/core';
-import { MatToolbar,MatToolbarModule  } from '@angular/material/toolbar';
+import { MatToolbarModule  } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule, MatNavList } from '@angular/material/list';
-import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import {  RouterModule } from '@angular/router';
 import { MenuComponent } from './views/common/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, 
+  imports: [MenuComponent, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule, 
     CommonModule, MatSidenavModule, MatListModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
