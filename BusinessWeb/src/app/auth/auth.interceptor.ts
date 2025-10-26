64a96f0a5ffdd1ab22e25f80219487d58fd4244a
karthikeyan-1234 <mail2karthikkn@gmail.com>
2025-10-26 17:07:12 +0530

    Added refresh token to token interceptor

--- BusinessWeb/src/app/auth/auth.interceptor.ts
+++ BusinessWeb/src/app/auth/auth.interceptor.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable, from } from 'rxjs';
-import { KeycloakService } from 'keycloak-angular';
 import { switchMap } from 'rxjs/operators';
+import { KeycloakService } from 'keycloak-angular';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   constructor(private keycloakService: KeycloakService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return from(this.keycloakService.getToken()).pipe(
+    return from(
+      // Refresh token if it will expire in the next 30 seconds
+      this.keycloakService.updateToken(30).then((refreshed ) => {
+        if (refreshed) {
+          console.log('Token was successfully refreshed');
+        } else {
+          console.log('Token is still valid');
+        }
+      })
+    ).pipe(
+      switchMap(() => from(this.keycloakService.getToken())),
       switchMap(token => {
         if (token) {
           const authReq = req.clone({
