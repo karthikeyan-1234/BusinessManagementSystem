22abfb2ebf68d2cd3623cf210c825c3fc6e60c73
karthikeyan-1234 <mail2karthikkn@gmail.com>
2025-10-26 13:37:36 +0530

    Integrating Keycloak - In progress

--- BusinessWeb/src/app/app.config.ts
+++ BusinessWeb/src/app/app.config.ts
@@ -1,13 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 import {provideNativeDateAdapter} from '@angular/material/core';
+import { KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from './services/keycloak-init';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    KeycloakService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      deps: [KeycloakService],
+      multi: true
+    },
     provideHttpClient(),
     provideNativeDateAdapter()
   ]
--- BusinessWeb/src/app/app.routes.ts
+++ BusinessWeb/src/app/app.routes.ts
@@ -6,13 +6,14 @@ import { PaymentsComponent } from './views/payments/payments.component';
 import { CheckoutComponent } from './views/checkout/checkout.component';
 import { PaymentSuccessComponent } from './views/payment-success/payment-success.component';
 import { OrderComponent } from './views/order/order.component';
+import { KeycloakGuard } from './auth/keycloak.guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/products', pathMatch: 'full' },
-    { path: 'products', component: ProductsComponent },
-    { path: 'inventory', component: InventoryComponent },
-    { path: 'orders', component: OrderComponent },
-    { path: 'payments', component: PaymentsComponent },
-    { path: 'checkout', component: CheckoutComponent },
-    { path: 'payment-success', component: PaymentSuccessComponent },
+    { path: '', redirectTo: '/products', pathMatch: 'full', canActivate: [KeycloakGuard] },
+    { path: 'products', component: ProductsComponent, canActivate: [KeycloakGuard] },
+    { path: 'inventory', component: InventoryComponent, canActivate: [KeycloakGuard] },
+    { path: 'orders', component: OrderComponent, canActivate: [KeycloakGuard] },
+    { path: 'payments', component: PaymentsComponent, canActivate: [KeycloakGuard] },
+    { path: 'checkout', component: CheckoutComponent, canActivate: [KeycloakGuard] },
+    { path: 'payment-success', component: PaymentSuccessComponent, canActivate: [KeycloakGuard] },
 ];
